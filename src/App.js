f99ed1bdb3f0f5a4e42bfeb0a86be6ec957cab43
bouchra.lamkadmi <bouchra.lamkadmi@sahamassurance.com>
2019-06-04 11:25:25 +0000

    call api and consume it by Repso Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Repos from './components/Repos.js'
 
 import './App.css';
 
-function App() {
-  return (
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      repos: [],
+    };
+  }
+  componentDidMount() {
+    fetch('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ repos: data.items })
+      })
+      .catch(console.log)
+  }
+  render() {
+    return (
     <div className="App">
-      <Repos />
+        <Repos repos={this.state.repos}/>
     </div>
   );
+  }
+  
 }
 
 export default App;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import avatar from './../avatar.png';
-
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import moment from 'moment';
 import './../App.css';
 
-const Repos = ({ Repos }) =>{
-    return (
-        <div className="container">
-            <img src={avatar} alt="avatar" width="100" height="100" />
-            <div className="wrapper">
-                <h1 className="header">test</h1>
-                <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum</p>
-                <span className="indicators">Stars: 11k</span>
-                <span className="indicators">Issues: 23</span>
-                <span className="information">Submitted 30 days ago by test</span>
-            </div>
-        </div>
-    );
+export class Repos extends Component {
+getDuration = (date) => {
+    return moment(date).diff(moment([2017, 11, 22]), 'days') ? 30 : '';
 }
-
+   render() {   
+       console.log(this.props.repos)
+    return (
+        this.props.repos.map((rep)=> 
+           <div className="container">
+                <img src={rep.owner.avatar_url} alt="avatar" width="100" height="100" />
+            <div className="wrapper">
+                <h1 className="header">{rep.name}</h1>
+                <p>{rep.description}</p>
+                    <span className="indicators">Stars: {rep.stargazers_count}</span>
+                    <span className="indicators">Issues: {rep.open_issues}</span>
+                    <span className="information">Submitted {this.getDuration(rep.created_at)} days ago by {rep.owner.login}</span>
+            </div>
+        </div>  
+        )
+       
+    );
+    }
+}
+Repos.propTypes = {
+  repos: PropTypes.array,
+}
 export default Repos;
